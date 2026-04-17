ae55e44c007930cad046d09ea788e24ccf004ba8
jumana-rahman <jumanarahman242@outlook.com>
2026-04-17 07:38:16 +0600

    added condition

--- src/components/statsAnalytics/AnalyticsChart.jsx
+++ src/components/statsAnalytics/AnalyticsChart.jsx
@@ -33,10 +33,10 @@ const AnalyticsChart = () => {
         Text: "#7E35E1",
         Video: "#37A163",
     };
+
     return (
         <div className='bg-[#F8FAFC]'>
             <div className="w-[80%] mx-auto py-20">
-                {/* Heading */}
                 <h1 className="text-[48px] font-bold mb-6 text-[#1F2937]">
                     Friendship Analytics
                 </h1>
@@ -45,7 +45,12 @@ const AnalyticsChart = () => {
                 <div className='p-4 bg-white border border-zinc-200 rounded-lg'>
                     <h3 className='text-[#244D3F] text-[20px] font-medium pb-5 capitalize'>by interaction type</h3>
 
+                    {data.every(item => item.value === 0) && (
+                        <p className="text-center text-gray-500 text-lg flex justify-center mt-4">No interactions yet. <br/> Pie Chart will show when there is interaction available.</p>
+                    )}
+
                     <div className="flex justify-center">
+                        
                         <PieChart
                         width={400}
                         height={400}
@@ -74,9 +79,7 @@ const AnalyticsChart = () => {
                     </div>
                 </div>
 
-                {data.every(item => item.value === 0) && (
-                    <p className="p-4 bg-white border border-zinc-200 rounded-lg">No interactions yet</p>
-                )}
+                
             </div>
         </div>
     );
